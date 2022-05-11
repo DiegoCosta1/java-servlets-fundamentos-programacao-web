33d03195722d6e615f454c54868c8c49d758d71e
DiegoCosta1 <deegancosta@gmail.com>
2022-05-10 21:06:52 -0300

    create new company servlet

---
+++ gerenciador/src/main/java/br/com/alura/gerenciador/servlet/NewCompanyServlet.java
@@ -0,0 +1,32 @@
+package br.com.alura.gerenciador.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class NewCompanyServlet
+ */
+@WebServlet("/newCompany")
+public class NewCompanyServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public NewCompanyServlet() {
+        super();
+    }
+
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String nomeEmpresa = req.getParameter("nome");
+		
+		PrintWriter out = resp.getWriter();
+		
+		out.println("Cadastrando nova empresa: " + nomeEmpresa + ".");
+		
+	}
+
+}
