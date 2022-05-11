f76c32e4cad34533eb18d30ce6d85449400c7ebb
DiegoCosta1 <deegancosta@gmail.com>
2022-05-10 23:02:30 -0300

    adding static companies

--- gerenciador/src/main/java/br/com/alura/gerenciador/model/Database.java
+++ gerenciador/src/main/java/br/com/alura/gerenciador/model/Database.java
@@ -6,6 +6,16 @@ import java.util.List;
 public class Database {
 	
 	private static List<Company> companies = new ArrayList<Company>();
+	
+	static {
+		Company alura = new Company();
+		alura.setName("Alura");
+		Company google = new Company();
+		google.setName("Google");
+		
+		companies.add(alura);
+		companies.add(google);
+	}
 
 	public void add(Company company) {
 		companies.add(company);
